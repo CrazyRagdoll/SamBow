a0c518aa0db60132030a73bdaecf75c00ee7fd9c
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2017-03-06 16:45:43 +0000

    Added drawGame function along with some GLew initialisations. Using depreicated OpenGL functions to draw a triangle.

--- Sambow/MainGame.cpp
+++ Sambow/MainGame.cpp
@@ -1,7 +1,15 @@
 #include "MainGame.h"
 
 #include <iostream>
+#include <string>
 
+void fatalError(std::string errorString) {
+	std::cout << errorString << std::endl;
+	std::cout << "Enter any key to quit...";
+	int tmp;
+	std::cin >> tmp;
+	SDL_Quit();
+}
 
 MainGame::MainGame()
 {
@@ -32,6 +40,26 @@ void MainGame::initSystems() {
 
 	//Create an SDL window
 	_window = SDL_CreateWindow("SamBow Game Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, _screenWidth, _screenHeight, SDL_WINDOW_OPENGL);
+	if (_window == nullptr) {
+		fatalError("SDL Window could no be created!");
+	}
+
+	//Set up our OpenGL context
+	SDL_GLContext glContext = SDL_GL_CreateContext(_window);
+	if (glContext == nullptr) {
+		fatalError("SDL_GL context could not be created!");
+	}
+
+	//Set up glew (optional but recommended)
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		fatalError("Could not initialize glew!");
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
 }
 
 
@@ -39,6 +67,7 @@ void MainGame::initSystems() {
 void MainGame::gameLoop(){
 	while (_gameState != GameState::EXIT) {
 		processInput();
+		drawGame();
 	}
 }
 
@@ -58,4 +87,22 @@ void MainGame::processInput() {
 			break;
 		}
 	}
+}
+
+void MainGame::drawGame() {		//Draw content to the game
+	glClearDepth(1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glEnableClientState(GL_COLOR_ARRAY);
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(0, 0);
+	glVertex2f(0, 500);
+	glVertex2f(500, 500);
+
+	glEnd();
+
+
+	SDL_GL_SwapWindow(_window);
 }
--- Sambow/MainGame.h
+++ Sambow/MainGame.h
@@ -1,4 +1,5 @@
 #pragma once  //Protects you from circular includes
+#include <Windows.h>
 
 #include <SDL/SDL.h>
 #include <GL/glew.h>
@@ -18,6 +19,7 @@ private:
 	void initSystems();		//Inialise everything needed to run the game
 	void gameLoop();		//The main game loop which will do all of the magic.
 	void processInput();	//Processing input from the user
+	void drawGame();		//Draw content to the game
 
 	SDL_Window* _window;
 	int _screenWidth;
