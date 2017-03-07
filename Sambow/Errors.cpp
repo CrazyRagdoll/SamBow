bf187c85a51fd820c3c8e2db4c6253ee72cd683f
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2017-03-07 16:43:31 +0000

    Added comments to GLSL and Errors cpp files. Fixed minor bug in GLSLProgram

--- Sambow/Errors.cpp
+++ Sambow/Errors.cpp
@@ -1,5 +1,7 @@
 #include "Errors.h"
 
+#include <cstdlib>
+
 #include <iostream>
 #include <SDL/SDL.h>
 
@@ -9,5 +11,6 @@ void fatalError(std::string errorString) {
 	std::cout << "Enter any key to quit...";
 	int tmp;
 	std::cin >> tmp;
-	SDL_Quit();
+	SDL_Quit();		//Close all SDL operations
+	exit(69);		//Close the program
 }
--- Sambow/GLSLProgram.cpp
+++ Sambow/GLSLProgram.cpp
@@ -17,26 +17,25 @@ GLSLProgram::~GLSLProgram()
 
 //Read our vertex and fragment shaders and compile them
 void GLSLProgram::compileShaders(const std::string& vertexShaderFilePath, const std::string& fragmentShaderFilePath) {
+	//Create the vertex shader object, and store its ID
 	_vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
 	if (_vertexShaderID == 0) {
 		fatalError("Vertex shader failed to be created!");
 	}
 
+	//Create the fragment shader object, and store its ID
 	_fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 	if (_fragmentShaderID == 0) {
 		fatalError("Fragment shader failed to be created!");
 	}
 
+	//compile each shader
 	compileShader(vertexShaderFilePath, _vertexShaderID);
 	compileShader(fragmentShaderFilePath, _fragmentShaderID);
-
 }
 
 //Link the shaders to the program
 void GLSLProgram::linkShaders() {
-	//Get the program object
-	_programID = glCreateProgram();
-
 	//Attach our shaders to our program
 	glAttachShader(_programID, _vertexShaderID);
 	glAttachShader(_programID, _fragmentShaderID);
@@ -75,13 +74,15 @@ void GLSLProgram::linkShaders() {
 	glDeleteShader(_fragmentShaderID);
 }
 
+//Adds an attribute to our shader, should be called between compiling and linking
 void GLSLProgram::addAttribute(const std::string& attributeName) {
 	glBindAttribLocation(_programID, _numAttributes++, attributeName.c_str());
 }
 
-
+//Enable the shader and all of its attributes
 void GLSLProgram::use() {
 	glUseProgram(_programID);
+	//Enable all the attrivutes we added with addAttribute
 	for (int i = 0; i < _numAttributes; i++) {
 		glEnableVertexAttribArray(i);
 	}
@@ -94,29 +95,39 @@ void GLSLProgram::unuse() {
 	}
 }
 
-//Compile the shaders
+//Compile the shader files
 void GLSLProgram::compileShader(const std::string& filePath, GLuint id) {
+	//Get the program object
+	_programID = glCreateProgram();
 
+	//Open the file
 	std::ifstream vertexFile(filePath);
 	if (vertexFile.fail()) {
 		perror(filePath.c_str());
 		fatalError("Failed to open " + filePath);
 	}
 
+	//File contents stores all the text in the file
 	std::string fileContents = "";
+	//Line is used to grab each line of the file
 	std::string line;
 
+	//Get all the lines in the file and add it to the contents
 	while (std::getline(vertexFile, line)) {
 		fileContents += line + "\n";
 	}
 
 	vertexFile.close();
 
+	//Get a pointer to our file contents c string
 	const char* contentsPtr = fileContents.c_str();
+	//tell opengl that we want to use fileContents as teh contents of the shader file
 	glShaderSource(id, 1, &contentsPtr, nullptr);
 
+	//compile the shader
 	glCompileShader(id);
 
+	//Check for errors
 	GLint success = 0;
 	glGetShaderiv(id, GL_COMPILE_STATUS, &success);
 
